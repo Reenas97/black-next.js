1b5a8cc8fb9216a271c256c37c348718fed13efe
Renata Dal Bello Sanchez <reenadbsanchez@gmail.com>
2024-03-20 15:54:48 -0300

    conteudo home e produto. inc produtos da api

---
+++ src/pages/components/ProductCard.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import Link from "next/link"
+import React, { useState } from "react"
+import { Button, Card, CardBody, CardSubtitle } from "reactstrap"
+import { ProductType } from "../services/products"
+import SuccessToast from "./SuccessToast"
+
+type ProductCardProps = {
+    product: ProductType
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+    const [toastIsOpen, setToastIsOpen] = useState(false)
+    const { id, name, imageUrl, price } = product
+  
+    return (
+        <>
+            <Card>
+                <Link href={`/products/${id}`}>
+                                                                                {/* elementos height e width s]ao usados para saber quais são as proproções da imagem. São obrigatorio */}  
+                    <Image className="card-img-top" src={imageUrl} alt={product.name} width={600} height={500} layout="responsive" />
+                </Link>
+
+                <CardBody>
+                    <Link className="card-title text-decoration-none" href={`/products/${id}`}>
+                      <h5 style={{ cursor: 'pointer' }}>
+                        {name}
+                      </h5>
+                    </Link>
+
+                    <CardSubtitle className="mb-3 text-muted" tag="h6">
+                      R$ {price}
+                    </CardSubtitle>
+
+                    <Button
+                      color="dark"
+                      className="pb-2"
+                      block
+                      onClick={() => {
+                        setToastIsOpen(true)
+                        setTimeout(() => setToastIsOpen(false), 1000 * 3)
+                      }}
+                    >
+                      Adicionar ao Carrinho
+                    </Button>
+                
+                  </CardBody>
+                </Card>
+                
+            <SuccessToast toastIsOpen={toastIsOpen} setToastIsOpen={setToastIsOpen} /> {/* cada vez que clicar no botão de adicionar ao carrinho, vai abrir o Toast e depois fechar o toast */}
+        </>
+    )
+  }
+  
+  export default ProductCard
---
+++ src/pages/services/products.ts
@@ -0,0 +1,21 @@
+//simular as chamadas à API
+// src/services/products.ts
+
+export type ProductType = {
+    id: number
+    name: string
+    description: string
+    price: number
+    imageUrl: string
+    inStock: number
+}
+  
+export const fetchProducts = async () => {
+    const products: ProductType[] = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/products`).then(res => res.json())
+    return products
+} //todos os produtos
+  
+export const fetchProduct = async (id: string | number) => {
+    const product: ProductType = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/products/${id}`).then(res => res.json())
+    return product
+} //produto especifico
